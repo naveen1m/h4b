853f165d5bd694e807dc389616cdfe838c5f218d
naveen <stupiddint@gmail.com>
2023-07-09 23:16:02 +0530

    bohot der se nai maare they

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -5,6 +5,8 @@ async function connect() {
     const mongodb = await MongoMemoryServer.create();
     const getUri = mongodb.getUri();
 
+    console.log(getUri);
+
     mongoose.set('strictQuery', true);
     const db = await mongoose.connect(getUri);
     console.log("Database connected");
--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const connect = require('./configs/mongodb');
+const authRouter = require('./router/authRouter');
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
     res.send('api working');
 })
+
+app.use('/auth', authRouter)
 
 connect().then(() => {
     app.listen(PORT, () => {
