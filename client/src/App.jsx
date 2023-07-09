9d05f55025587b90858432303fdbbb7e4505fc2a
Dishant Yadav <dishantyadav599@gmail.com>
2023-07-09 15:46:49 +0530

    Setup mantineui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import "./index.css";
+import { Button } from "@mantine/core";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-green-700">
+      <h1 className="text-3xl font-bold underline text-yellow-700">
         Hello world!
       </h1>
+      <Button>Settings</Button>
     </div>
   );
+
 }
 
 export default App;
